12f951845f5ad5cf06dae5e7f64dc93ecbed0210
Chony <chonyor76@gmail.com>
2026-07-27 22:12:41 +0800

    changes cors

--- config/cors.php
+++ config/cors.php
@@ -2,18 +2,13 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['https://task-management-react-nine.vercel.app'],
-
+    'allowed_origins' => [
+        'https://task-management-react-nine.vercel.app',
+    ],
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
