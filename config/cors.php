e47e07ff4b9f10e99a0dc7e459b482ae6502dae0
Chony <chonyor76@gmail.com>
2026-07-27 22:31:01 +0800

    trying to fix bug cors

--- config/cors.php
+++ config/cors.php
@@ -5,8 +5,12 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'https://task-management-react-nine.vercel.app',
+        'http://localhost:3000',
+        'http://localhost:5173',
     ],
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '/^https:\/\/.*\.vercel\.app$/',
+    ],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
